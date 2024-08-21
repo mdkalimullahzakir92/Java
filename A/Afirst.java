863b675fab7277af2cf128b90989c2b0bc930332
mdkalimullahzakir92 <haiderzakir92@gmail.com>
2024-08-21 11:58:44 +0530

    Update Afirst.java

--- A/Afirst.java
+++ A/Afirst.java
@@ -1,1 +1,13 @@
+class Afirst{
+        public static void main(String[] args){
+                System.out.println("BISMILLAH-IR-RAHMAAN-IR-RAHEEM");
+        }
+}
+/*
+        To execute this file on the command Prompt type the following command:
+        javac Afirst.java  // to compile
+        java Afirst        // to run
 
+        To execute directly without using run button:
+        press ctrl + alt + N
+*/
